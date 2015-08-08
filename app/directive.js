193bf28135cd9599fcf80e332b6495d01f51b356
Luke Engelhardt <lukestir@gmail.com>
2015-08-08 13:19:11 -0600

    saving work

--- app/directive.js
+++ app/directive.js
@@ -1,7 +1,13 @@
 var app = angular.module('directivePractice').directive('nglDisplay', function() {
 
     return {
-        templateUrl: 'app/nglTemplate.html'
+        templateUrl: 'app/nglTemplate.html',
+        link: function(scope, element, attr) {
+            element.on('click', function() {
+                scope.showMe = true;
+                scope.$apply();
+            })
+        }
     }
 
 
